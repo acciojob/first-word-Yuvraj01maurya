e773aca872a1e4b171ba4719a637e0a8409b6a8f
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-16 22:00:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	if (!s || !s.includes(' ')) {
+    // If the input string is empty or does not contain any space, return the entire string.
+    return s;
+  }
+  // Find the index of the first space in the input string.
+  const index = s.indexOf(' ');
+  // Return the substring from the beginning of the string to the index of the first space.
+  return s.substring(0, index);
 }
 
 // Do not change the code below
